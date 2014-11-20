e442afa9bb63c1d245aaf039d7a774effd85fc63
Philip Molloy <philip.a.molloy@gmail.com>
2014-11-19 20:01:57 -0500

    Rename FILE to FILENAME

--- rename-photo.bash
+++ rename-photo.bash
@@ -16,32 +16,32 @@
 #       $ rename-photo.bash *.jpg
 #
 
-for FILE in "$@"; do
-    echo Processing $FILE
-    LINE=$(identify -verbose "$FILE" | grep "DateTime:")
+for FILENAME in "$@"; do
+    echo Processing $FILENAME
+    LINE=$(identify -verbose "$FILENAME" | grep "DateTime:")
     echo EXIF output: $LINE
     if [ -z "$LINE" ]; then
-        echo ERROR: Datetime not found in $FILE
+        echo ERROR: Datetime not found in $FILENAME
     else
         IFS=$' '
         read -a ARRAY <<< "${LINE:4}"   # Split on ` ` and remove first 4 chars
         DATE="${ARRAY[1]}"
         TIME="${ARRAY[2]}"
-        if [ -z "$TIME" ]; then echo ERROR: Time not found in $FILE; break; fi
+        if [ -z "$TIME" ]; then echo ERROR: Time not found in $FILENAME; break; fi
         echo Time: $TIME
         IFS=$':'
         read -a ARRAY <<< "${DATE}"
         YEAR="${ARRAY[0]}"
         echo Year: $YEAR
-        if [ -z "$YEAR" ]; then echo ERROR: Year not found in $FILE; break; fi
+        if [ -z "$YEAR" ]; then echo ERROR: Year not found in $FILENAME; break; fi
         MONTH="${ARRAY[1]}"
         echo Month: $MONTH
-        if [ -z "$MONTH" ]; then echo ERROR: Month not found in $FILE; break; fi
+        if [ -z "$MONTH" ]; then echo ERROR: Month not found in $FILENAME; break; fi
         DAY="${ARRAY[2]}"
         echo Day: $DAY
-        if [ -z "$DAY" ]; then echo ERROR: Day not found in $FILE; break; fi
+        if [ -z "$DAY" ]; then echo ERROR: Day not found in $FILENAME; break; fi
         EPOCH="$(date --date="$MONTH/$DAY/$YEAR $TIME" "+%s")"
-        EXTENSION="${FILE##*.}"
+        EXTENSION="${FILENAME##*.}"
         mv $FILE "$EPOCH.$EXTENSION"
     fi
 done
