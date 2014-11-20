6e274032db44fcc31391b739ca2f0ba2f0ef2f76
Philip Molloy <philip.a.molloy@gmail.com>
2014-11-19 20:01:00 -0500

    Add error handling and logging

--- rename-photo.bash
+++ rename-photo.bash
@@ -17,7 +17,9 @@
 #
 
 for FILE in "$@"; do
+    echo Processing $FILE
     LINE=$(identify -verbose "$FILE" | grep "DateTime:")
+    echo EXIF output: $LINE
     if [ -z "$LINE" ]; then
         echo ERROR: Datetime not found in $FILE
     else
@@ -25,11 +27,19 @@ for FILE in "$@"; do
         read -a ARRAY <<< "${LINE:4}"   # Split on ` ` and remove first 4 chars
         DATE="${ARRAY[1]}"
         TIME="${ARRAY[2]}"
+        if [ -z "$TIME" ]; then echo ERROR: Time not found in $FILE; break; fi
+        echo Time: $TIME
         IFS=$':'
         read -a ARRAY <<< "${DATE}"
         YEAR="${ARRAY[0]}"
+        echo Year: $YEAR
+        if [ -z "$YEAR" ]; then echo ERROR: Year not found in $FILE; break; fi
         MONTH="${ARRAY[1]}"
+        echo Month: $MONTH
+        if [ -z "$MONTH" ]; then echo ERROR: Month not found in $FILE; break; fi
         DAY="${ARRAY[2]}"
+        echo Day: $DAY
+        if [ -z "$DAY" ]; then echo ERROR: Day not found in $FILE; break; fi
         EPOCH="$(date --date="$MONTH/$DAY/$YEAR $TIME" "+%s")"
         EXTENSION="${FILE##*.}"
         mv $FILE "$EPOCH.$EXTENSION"
